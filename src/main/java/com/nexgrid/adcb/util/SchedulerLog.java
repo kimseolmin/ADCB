8f9653f5140be86f9fcc5e1d3a1aa1da76ca1059
yjkim <yjkim@1fb4f7e9-be3c-4d76-afcf-f5a8979d8c97>
2019-06-13 09:37:57 +0000

    git-svn-id: http://nexgrid2.iptime.org:8880/svn/adcb/trunk/ADCB.BokuAPI@666 1fb4f7e9-be3c-4d76-afcf-f5a8979d8c97

--- src/main/java/com/nexgrid/adcb/util/SchedulerLog.java
+++ src/main/java/com/nexgrid/adcb/util/SchedulerLog.java
@@ -8,12 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+
 public class SchedulerLog extends TimerTask {
 	
 	static private long lastTime = 0;
 	
 	//oms log => log4j2 
 	private static org.apache.logging.log4j.core.Logger omsLog = (org.apache.logging.log4j.core.Logger)LogManager.getLogger("oms");
+	private static final Logger logger = LoggerFactory.getLogger(SchedulerLog.class);
 	
 	//Date now;
 	
@@ -27,6 +29,7 @@ public class SchedulerLog extends TimerTask {
 			
 			//oms가 찍히지 않더라도 빈파일을 생성해주기 위함.
 			omsLog.info("");
+			logger.info(">>>>>>>>>>>>>>>>>>>>>>>> Keep Alive <<<<<<<<<<<<<<<<<<<<<<<<<");
 			
 			lastTime = currTime;
 		}
